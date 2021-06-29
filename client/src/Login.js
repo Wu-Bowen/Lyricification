38285612405f08adbcb6e7024b64dab8738d9431
Eric Wu <ericwu692@yahoo.com>
2021-06-29 12:12:28 -0400

    added authorization

---
+++ client/src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize?clientid=dc98d5777aaa4443b1e2218303fa630c&response_type=code&redirect_uri=https://localhost:3001&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+export default function Login() {
+    return (
+        <div>
+
+        </div>
+    )
+}
